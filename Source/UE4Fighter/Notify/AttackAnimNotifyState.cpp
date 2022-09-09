17a67c89883cfd63d3c21796263356f5f2de6a33
AlexMercer <redownloader999@gmail.com>
2022-09-09 20:47:08 +0300

    clean debug messages

--- Source/UE4Fighter/Notify/AttackAnimNotifyState.cpp
+++ Source/UE4Fighter/Notify/AttackAnimNotifyState.cpp
@@ -5,8 +5,7 @@
 #include "../Character/UE4FighterCharacter.h"
 
 void UAttackAnimNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration) {
- GEngine->AddOnScreenDebugMessage(-1, 4.f, FColor::Cyan, __FUNCTION__);
- 
+ //GEngine->AddOnScreenDebugMessage(-1, 4.f, FColor::Cyan, __FUNCTION__);
  if (MeshComp != NULL && MeshComp->GetOwner() != NULL)
  {
   AUE4FighterCharacter* Player = Cast<AUE4FighterCharacter>(MeshComp->GetOwner());
@@ -18,7 +17,7 @@ void UAttackAnimNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnim
 }
 
 void UAttackAnimNotifyState::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) {
- GEngine->AddOnScreenDebugMessage(-1, 4.f, FColor::Cyan, __FUNCTION__);
+ //GEngine->AddOnScreenDebugMessage(-1, 4.f, FColor::Cyan, __FUNCTION__);
  if (MeshComp != NULL && MeshComp->GetOwner() != NULL)
  {
   AUE4FighterCharacter* Player = Cast<AUE4FighterCharacter>(MeshComp->GetOwner());
