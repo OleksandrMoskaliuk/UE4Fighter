45476d5d8a05841b08e6cd8f2e93a513c273b9f0
AlexMercer <redownloader999@gmail.com>
2022-09-14 17:51:00 +0300

    Player widget

---
+++ Source/UE4Fighter/UI/ComboWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ComboWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4FIGHTER_API UComboWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
