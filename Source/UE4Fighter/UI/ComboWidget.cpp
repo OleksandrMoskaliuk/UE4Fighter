5e35869c2c21261c822fd684b9dbd5a9368e5fa6
AlexMercer <redownloader999@gmail.com>
2022-09-15 12:18:04 +0300

    adjustments

--- Source/UE4Fighter/UI/ComboWidget.cpp
+++ Source/UE4Fighter/UI/ComboWidget.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "../UI/ComboWidget.h"
 #include "Animation/WidgetAnimation.h"
+#include "../UI/ComboWidget.h"
 
 
 UComboWidget::UComboWidget(const FObjectInitializer& ObjectInitializer) : UUserWidget(ObjectInitializer) {
