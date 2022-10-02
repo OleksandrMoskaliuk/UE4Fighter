81f4e6632158db9508e76f08b8dfdc9f7e8deebe
AlexMercer <redownloader999@gmail.com>
2022-10-02 17:03:33 +0300

    Spline actor override point structure

--- Source/UE4Fighter/Props/SplineProps/SplineActor.cpp
+++ Source/UE4Fighter/Props/SplineProps/SplineActor.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/SplineMeshComponent.h"
 #include "SplineActor.h"
 
 // Sets default values
@@ -29,7 +28,7 @@ void ASplineActor::Tick(float DeltaTime) {
 
 void ASplineActor::OnConstruction(const FTransform& Transform) {
 	AActor::OnConstruction(Transform);
-	if (SplineComponent->GetNumberOfSplinePoints() < 1) { return; }
+	if (!DefaultMesh) { return; } // if there no mesh no need to go farther
 	for (int i = 0; i < SplineComponent->GetNumberOfSplinePoints() - 1; i++)
 	{
 		// Get  vectors for making meshes on certain points
@@ -39,14 +38,38 @@ void ASplineActor::OnConstruction(const FTransform& Transform) {
 		FVector NextTangent = SplineComponent->GetTangentAtSplinePoint(i + 1, ESplineCoordinateSpace::Local);
 		// Making meshes on spline points
 		USplineMeshComponent* SplineMeshComponent = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass());
-		SplineMeshComponent->SetStaticMesh(Mesh);
-		SplineMeshComponent->SetMobility(EComponentMobility::Movable);
-		SplineMeshComponent->CreationMethod = EComponentCreationMethod::UserConstructionScript;
-		SplineMeshComponent->RegisterComponentWithWorld(GetWorld());
-		SplineMeshComponent->AttachToComponent(SplineComponent, FAttachmentTransformRules::KeepRelativeTransform);
-		//Locate mesh in the right position 
-		SplineMeshComponent->SetStartAndEnd(StartPoint, StartTangent, NextPoint, NextTangent, true);
-		SplineMeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		if (SplineMeshComponent)
+		{
+			SplineMeshComponent->SetStaticMesh(DefaultMesh);
+			SplineMeshComponent->SetMobility(EComponentMobility::Movable);
+			SplineMeshComponent->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+			SplineMeshComponent->RegisterComponentWithWorld(GetWorld());
+			SplineMeshComponent->AttachToComponent(SplineComponent, FAttachmentTransformRules::KeepRelativeTransform);
+			//Locate mesh in the right position 
+			SplineMeshComponent->SetStartAndEnd(StartPoint, StartTangent, NextPoint, NextTangent, true);
+			SplineMeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+			SplineMeshComponent->SetForwardAxis(SplineMeshAxis);
+			// Get matirial from MeshMaterialInstanceMap else use default material
+			if (SplineOverrideMap.Contains(i))
+			{
+				//Set Mesh
+				if (SplineOverrideMap.Find(i)->Mesh) 
+				{
+				SplineMeshComponent->SetStaticMesh(SplineOverrideMap.Find(i)->Mesh);
+				}
+				// Set Material
+				if (SplineOverrideMap.Find(i)->MaterialInstance)
+				{
+					SplineMeshComponent->SetMaterial(0, SplineOverrideMap.Find(i)->MaterialInstance);
+				}
+				// Set Axis
+				SplineMeshComponent->SetForwardAxis(SplineOverrideMap.Find(i)->SplineMeshAxis);
+			}
+			else if (DefaultMaterialInstance)
+			{
+				SplineMeshComponent->SetMaterial(0, DefaultMaterialInstance);
+			}
+		}
 	}
 }
 
--- Source/UE4Fighter/Props/SplineProps/SplineActor.h
+++ Source/UE4Fighter/Props/SplineProps/SplineActor.h
@@ -5,19 +5,55 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Components/SplineComponent.h"
+#include "Components/SplineMeshComponent.h"
+#include "Engine/DataTable.h"
 
 #include "SplineActor.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSplineDetails : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	/** Mesh */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spline")
+		UStaticMesh* Mesh;
+
+	/** Mesh orientation  */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spline")
+		TEnumAsByte<ESplineMeshAxis::Type> SplineMeshAxis;
+
+	/** Material */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spline")
+		UMaterialInstance* MaterialInstance;
+
+	FSplineDetails() :SplineMeshAxis(ESplineMeshAxis::Type::Z) {}
+
+};
 
 UCLASS()
 class UE4FIGHTER_API ASplineActor : public AActor
 {
 	GENERATED_BODY()
 
+	/** Spline sequence */
 	UPROPERTY(VisibleAnywhere, Category = "Spline", meta = (AllowPrivateAccess = "true"))
 		USplineComponent* SplineComponent;
 
+	/** Default static mesh */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spline", meta = (AllowPrivateAccess = "true"))
-		UStaticMesh* Mesh;
+		UStaticMesh* DefaultMesh;
+
+	/** Default mesh orientation  */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spline", meta = (AllowPrivateAccess = "true"))
+		TEnumAsByte<ESplineMeshAxis::Type> SplineMeshAxis;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spline", meta = (AllowPrivateAccess = "true"))
+		UMaterialInstance* DefaultMaterialInstance;
+
+	/** In case we need override some of the spline meshes */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Spline", meta = (AllowPrivateAccess = "true"))
+		TMap<int, FSplineDetails> SplineOverrideMap;
 
 public:
 	// Sets default values for this character's properties
@@ -26,8 +62,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
-public:	
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
